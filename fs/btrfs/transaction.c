aba18faa659c654d2311424e4455020bbe01ee86
Sun YangKai <sunk67188@gmail.com>
2026-02-01 22:32:30 +0800

    btrfs: remove unnecessary check in update_cowonly_root()
    fix 86b9f2eca5e0984145e3c7698a7cd6dd65c2a93f

--- fs/btrfs/transaction.c
+++ fs/btrfs/transaction.c
@@ -1327,16 +1327,12 @@ static int update_cowonly_root(struct btrfs_trans_handle *trans,
 {
 	int ret;
 	u64 old_root_bytenr;
-	u64 old_root_used;
 	struct btrfs_fs_info *fs_info = root->fs_info;
 	struct btrfs_root *tree_root = fs_info->tree_root;
 
-	old_root_used = btrfs_root_used(&root->root_item);
-
 	while (1) {
 		old_root_bytenr = btrfs_root_bytenr(&root->root_item);
-		if (old_root_bytenr == root->node->start &&
-		    old_root_used == btrfs_root_used(&root->root_item))
+		if (old_root_bytenr == root->node->start)
 			break;
 
 		btrfs_set_root_node(&root->root_item, root->node);
@@ -1345,8 +1341,6 @@ static int update_cowonly_root(struct btrfs_trans_handle *trans,
 					&root->root_item);
 		if (ret)
 			return ret;
-
-		old_root_used = btrfs_root_used(&root->root_item);
 	}
 
 	return 0;
